654acc3080afd7000ce535e5b168b278e88d58fe
Baranov_Vladimir <baranov.vladimir1990@hotmail.com>
2022-04-17 10:52:50 +0400

    Initial commit

---
+++ src/main/java/ru/gb/gbshopproject/GbShopProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.gbshopproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbShopProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GbShopProjectApplication.class, args);
+    }
+
+}
